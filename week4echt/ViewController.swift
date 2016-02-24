9d0b417e8bb8ce4f66125d91e8f50f7b3b4ba35e
bob <bobmbp@wcw-staff-145-18-165-77.wireless.uva.nl>
2016-02-24 14:51:50 +0100

    Initial Commit

---
+++ week4echt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week4echt
+//
+//  Created by bob on 24/02/16.
+//  Copyright © 2016 bob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
