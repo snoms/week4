85715ba8106a46f2c7ae2bfb96dec831c6f1fd5c
bob <bobmbp@bobs-macbook-pro.local>
2016-02-27 02:33:02 +0100

    firstworkingversion

--- week4echt/ViewController.swift
+++ week4echt/ViewController.swift
@@ -8,18 +8,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.inputTodo.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    @IBOutlet weak var inputTodo: UITextField!
+    
+    var todoArray = ["test", "test2", "test3"]
+    @IBAction func enterTodo(sender: AnyObject) {
+        if (inputTodo.text == "") {
+            //1. Create the alert controller.
+            var alert = UIAlertController(title: "You didn't enter anything!", message: "Insert your todo here or press OK to insert an empty row", preferredStyle: .Alert)
+            
+            //2. Add the text field. You can configure it however you need.
+            alert.addTextFieldWithConfigurationHandler({ (textField) -> Void in
+                textField.text = ""
+            })
 
+            //3. Grab the value from the text field, and print it when the user clicks OK.
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+                let textField = alert.textFields![0] as UITextField
+                print("Text field: \(textField.text)")
+                self.todoArray.append(textField.text!)
+                self.tableView.reloadData()
+            }))
+            var cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) {
+                UIAlertAction in
+                NSLog("Cancel Pressed")
+            }            // 4. Present the alert.
+            alert.addAction(cancelAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+            
+        }
+        else {
+            // alert: you have entered an empty todo. do you want to insert a white line?
+            // if yes --> continue, if no --> abort
+            todoArray.append(inputTodo.text!)
+            print(todoArray)
+            inputTodo.text = ""
+            tableView.reloadData()
+        }
+    }
+        
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! TodoCell
+        cell.todoTextfield.text = todoArray[indexPath.row]
+        cell.layoutMargins = UIEdgeInsetsZero
+        return cell
+    }
 
+    func textFieldShouldReturn(inputTodo: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            todoArray.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
+    
 }
 
