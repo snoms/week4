b9ce12e9d7342f24b8f1fa0f22e03dce3cac972c
snoms <tweaks@gmail.com>
2016-02-28 21:44:13 +0100

    working version 0.1.0

---
+++ README.md
@@ -0,0 +1,22 @@
+# To-do Table
+
+To-do Table is a very simple iOS application to manage your daily tasks.
+
+#### Features
+
+- Add, view and delete tasks on one screen
+- Remove tasks by swiping left
+- Alert window allows To-do input
+
+#### Bugs / missing features
+No data persistence yet. Needs a way to prioritize To-dos. Large To-dos are not completely displayed. Keyboard is slow to hide.
+
+
+#### Credits
+IQKeyboardManager
+
+Alert box with input field, taken from http://stackoverflow.com/questions/26567413/how-to-get-input-value-from-textfield-in-alert
+
+
+#### Version
+0.1.0
--- week4echt/ViewController.swift
+++ week4echt/ViewController.swift
@@ -15,12 +15,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         self.inputTodo.delegate = self
+        tableView.rowHeight = UITableViewAutomaticDimension
         // Do any additional setup after loading the view, typically from a nib.
+        /*if let directory: String = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true).first {
+            
+            let path: String = directory + "test.txt"
+            
+
+            
+            // Reading.
+            do {
+                let loadedArray = try String(contentsOfFile: path, encoding: NSUTF8StringEncoding)
+            } catch {
+                // Error handling here.
+            }
+        } else {
+            // Error handling here.
+        } */
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        /* Writing.
+        do {
+            try todoArray.writeToFile(path, atomically: false, encoding: NSUTF8StringEncoding)
+        }
+        catch {
+            // Error handling here.
+        } */
     }
     
     @IBOutlet weak var tableView: UITableView!
@@ -31,15 +56,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var todoArray = ["test", "test2", "test3"]
     @IBAction func enterTodo(sender: AnyObject) {
         if (inputTodo.text == "") {
-            //1. Create the alert controller.
+            // Create the alert controller.
             var alert = UIAlertController(title: "You didn't enter anything!", message: "Insert your todo here or press OK to insert an empty row", preferredStyle: .Alert)
             
-            //2. Add the text field. You can configure it however you need.
+            // Add the text field. You can configure it however you need.
             alert.addTextFieldWithConfigurationHandler({ (textField) -> Void in
                 textField.text = ""
             })
 
-            //3. Grab the value from the text field, and print it when the user clicks OK.
+            // Grab the value from the text field, and print it when the user clicks OK.
             alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
                 let textField = alert.textFields![0] as UITextField
                 print("Text field: \(textField.text)")
@@ -49,7 +74,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             var cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) {
                 UIAlertAction in
                 NSLog("Cancel Pressed")
-            }            // 4. Present the alert.
+                self.inputTodo.resignFirstResponder()
+            }            // Present the alert.
             alert.addAction(cancelAction)
             self.presentViewController(alert, animated: true, completion: nil)
             
